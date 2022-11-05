e0f89c5d0e4c0fbe863d7345e5d6cefa3c0f72f7
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2022-11-05 22:40:49 +0900

    dame

--- 20221105/c.py
+++ 20221105/c.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
-import itertools
+def searchMax(p, index, max):
+    maxValue = 0
+    resultIndex = 0
+    for i in range(index, len(p)):
+        if(p[i] > maxValue and p[i] < max):
+            maxValue = p[index]
+            result = index
+
+    return resultIndex
 
 # 整数の入力
 n = int(input())
-p = tuple(map(int, input().split()))
+p = list(map(int, input().split()))
 
 baseList = list(range(n+1))
 baseList.pop(0)
 
-prev = ()
-for i in itertools.permutations(baseList):
-    if p == i:
-        print(" ".join(map(str, prev)))
-        break
-    else:
-        prev = i
+for i in range(len(p)):
+    index = len(p) - i-1
+    preIndex = index -1
+    
+    if(p[preIndex] > p[index]):
+        max = searchMax(p, index, p[preIndex])
+        tmp = p[preIndex]
+        p[preIndex] = p[index]
+        p[index] = tmp
+
+print(p)
