048401fef4756b41f8c9fcdd3a0fa987f963eb75
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2022-12-04 20:28:47 +0900

    時間切れ

--- abc280/d.py
+++ abc280/d.py
@@ -1,25 +1,48 @@
 # -*- coding: utf-8 -*-
 import math
 
-def is_prime(n):
-    if n == 1:
-        return True
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+def factorization(n):
+    arr = []
+    temp = n
+    for i in range(2, int(-(-n**0.5//1))+1):
+        if temp%i==0:
+            cnt=0
+            while temp%i==0:
+                cnt+=1
+                temp //= i
+            arr.append([i, cnt])
+
+    if temp!=1:
+        arr.append([temp, 1])
+
+    if arr==[]:
+        arr.append([n, 1])
+
+    return arr
 
 k = int(input())
+fact = factorization(k)
+loopCount = 1
+for i in fact:
+    #print(i)
+    loopCount *= (i[1]+1)
 
-if is_prime(k):
-    print(k)
-    exit(0)
+values = []
+for i in range(loopCount):
+    #print("i=",i)
+    value = 1
+    tmpi = i
+    for j in range(len(fact)):
+        indexJ = tmpi % (fact[j][1]+1)
+        tmpi //= (fact[j][1]+1)
+        value *= fact[j][0] ** indexJ
+        #print("indexJ=", indexJ, "value=", value)
+    values.append(value)
 
-n = 1
-while True:
-    f = math.factorial(n)
-    if f % k == 0:
+sortedValues = sorted(values)
+#print(sortedValues)
+
+for i in range(len(sortedValues)):
+    if sortedValues[i] >= k and sortedValues % k == 0:
+        print(sortedValues[i])
         break
-    n += 1
-
-print(n)
