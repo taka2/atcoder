42d8f4caa8d28cb2defe52fc642e239d7e94569f
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2023-01-02 09:38:17 +0900

    Accepted!

--- abc258/b.py
+++ abc258/b.py
@@ -1,96 +1,60 @@
 # -*- coding: utf-8 -*-
-import sys
-sys.setrecursionlimit(10000000)
-
 N = int(input())
 A = []
 for i in range(N):
     A.append(list(map(int, input())))
-#print(N, A)
 
-# 盤面上の最大値を特定する
-maxNumber = 0
-for i in range(N):
-    for j in range(N):
-        if A[i][j] > maxNumber:
-            maxNumber = A[i][j]
+def getZahyoList(baseZahyo, tate, yoko):
+    resultList = [baseZahyo]
 
-# 盤面上の最大値を持つ座標を特定する（複数の可能性あり）
-maxNumberZahyoList = []
-for i in range(N):
-    for j in range(N):
-        if A[i][j] == maxNumber:
-            maxNumberZahyoList.append((i,j))
-#print(maxNumberZahyoList)
-
-
-def search(path):
-    if len(path) == N:
-        return path
-
-    lastZahyo = path[-1]
-    prevZahyo = None
-    if len(path) >= 2:
-        prevZahyo = path[-2]
-
-    upZahyo = ((lastZahyo[0]-1+N)%N, lastZahyo[1])
-    upValue = A[upZahyo[0]][upZahyo[1]] if upZahyo != prevZahyo else 0
-    downZahyo = ((lastZahyo[0]+1)%N, lastZahyo[1])
-    downValue = A[downZahyo[0]][downZahyo[1]] if downZahyo != prevZahyo else 0
-    leftZahyo = (lastZahyo[0], (lastZahyo[1]-1+N)%N)
-    leftValue = A[leftZahyo[0]][leftZahyo[1]] if leftZahyo != prevZahyo else 0
-    rightZahyo = (lastZahyo[0], (lastZahyo[1]+1)%N)
-    rightValue = A[rightZahyo[0]][rightZahyo[1]] if rightZahyo != prevZahyo else 0
-    upLeftZahyo = ((lastZahyo[0]-1+N)%N, (lastZahyo[1]-1+N)%N)
-    upLeftValue = A[upLeftZahyo[0]][upLeftZahyo[1]] if upLeftZahyo != prevZahyo else 0
-    upRightZahyo = ((lastZahyo[0]-1+N)%N, (lastZahyo[1]+1)%N)
-    upRightValue = A[upRightZahyo[0]][upRightZahyo[1]] if upRightZahyo != prevZahyo else 0
-    downLeftZahyo = ((lastZahyo[0]+1)%N, (lastZahyo[1]-1+N)%N)
-    downLeftValue = A[downLeftZahyo[0]][downLeftZahyo[1]] if downLeftZahyo != prevZahyo else 0
-    downRightZahyo = ((lastZahyo[0]+1)%N, (lastZahyo[1]+1)%N)
-    downRightValue = A[downRightZahyo[0]][downRightZahyo[1]] if downRightZahyo != prevZahyo else 0
-
-    resultList = []
-    maxValue = max(upValue, downValue, leftValue, rightValue, upLeftValue, upRightValue, downLeftValue, downRightValue)
-    print(path, lastZahyo, upZahyo, downZahyo, leftZahyo, rightZahyo, upLeftZahyo, upRightZahyo, downLeftZahyo, downRightZahyo, maxValue)
-    if upValue == maxValue and upZahyo != prevZahyo:
-        path.append(upZahyo)
-        resultList.append(search(path))
-        path.pop()
-    if downValue == maxValue and downZahyo != prevZahyo:
-        path.append(downZahyo)
-        resultList.append(search(path))
-        path.pop()
-    if leftValue == maxValue and leftZahyo != prevZahyo:
-        path.append(leftZahyo)
-        resultList.append(search(path))
-        path.pop()
-    if rightValue == maxValue and rightZahyo != prevZahyo:
-        path.append(rightZahyo)
-        resultList.append(search(path))
-        path.pop()
-    if upLeftValue == maxValue and upLeftZahyo != prevZahyo:
-        path.append(upLeftZahyo)
-        resultList.append(search(path))
-        path.pop()
-    if upRightValue == maxValue and upRightZahyo != prevZahyo:
-        path.append(upRightZahyo)
-        resultList.append(search(path))
-        path.pop()
-    if downLeftValue == maxValue and downLeftZahyo != prevZahyo:
-        path.append(downLeftZahyo)
-        resultList.append(search(path))
-        path.pop()
-    if downRightValue == maxValue and downRightZahyo != prevZahyo:
-        path.append(downRightZahyo)
-        resultList.append(search(path))
-        path.pop()
+    zahyo = baseZahyo
+    for i in range(N-1):
+        zahyo = ((zahyo[0]+tate+N)%N, (zahyo[1]+yoko+N)%N)
+        resultList.append(zahyo)
     return resultList
 
-# 各最大値座標から探索
-resultList = []
-for maxZahyo in maxNumberZahyoList:
-    currentPath = [maxZahyo]
-    resultList.extend(search(currentPath))
+def getValueFromZahyoList(zahyoList):
+    resultValue = 0
+    for i in range(N):
+        value = A[zahyoList[i][0]][zahyoList[i][1]]
+        resultValue += (value * 10**(N-i-1))
+    return resultValue
 
-print(resultList)
+result = 0
+for i in range(N):
+    for j in range(N):
+        baseZahyo = (i,j)
+        upZahyoList = getZahyoList(baseZahyo, -1, 0)
+        upValue = getValueFromZahyoList(upZahyoList)
+        if upValue > result:
+            result = upValue
+        downZahyoList = getZahyoList(baseZahyo, 1, 0)
+        downValue = getValueFromZahyoList(downZahyoList)
+        if downValue > result:
+            result = downValue
+        leftZahyoList = getZahyoList(baseZahyo, 0, -1)
+        leftValue = getValueFromZahyoList(leftZahyoList)
+        if leftValue > result:
+            result = leftValue
+        rightZahyoList = getZahyoList(baseZahyo, 0, 1)
+        rightValue = getValueFromZahyoList(rightZahyoList)
+        if rightValue > result:
+            result = rightValue
+        upLeftZahyoList = getZahyoList(baseZahyo, -1, -1)
+        upLeftValue = getValueFromZahyoList(upLeftZahyoList)
+        if upLeftValue > result:
+            result = upLeftValue
+        upRightZahyoList = getZahyoList(baseZahyo, -1, 1)
+        upRightValue = getValueFromZahyoList(upRightZahyoList)
+        if upRightValue > result:
+            result = upRightValue
+        downLeftZahyoList = getZahyoList(baseZahyo, 1, -1)
+        downLeftValue = getValueFromZahyoList(downLeftZahyoList)
+        if downLeftValue > result:
+            result = downLeftValue
+        downRightZahyoList = getZahyoList(baseZahyo, 1, 1)
+        downRightValue = getValueFromZahyoList(downRightZahyoList)
+        if downRightValue > result:
+            result = downRightValue
+
+print(result)
