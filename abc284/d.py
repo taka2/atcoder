0fa1804d2b3100f411998fd9f40aff7c960e4779
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2023-01-08 08:13:33 +0900

    Accepted!

--- abc284/d.py
+++ abc284/d.py
@@ -1,30 +1,20 @@
 # -*- coding: utf-8 -*-
-def factorization(n):
-    arr = []
-    temp = n
-    for i in range(2, int(-(-n**0.5//1))+1):
-        if temp%i==0:
-            cnt=0
-            while temp%i==0:
-                cnt+=1
-                temp //= i
-            arr.append([i, cnt])
-
-    if temp!=1:
-        arr.append([temp, 1])
-
-    if arr==[]:
-        arr.append([n, 1])
-
-    return arr
+import math
 
 T = int(input())
-for i in range(T):
-    fact = factorization(int(input()))
-    fact0 = fact[0]
-    fact1 = fact[1]
-    if fact0[1] == 2:
-        print(fact0[0], fact1[0])
-    else:
-        print(fact1[0], fact0[0])
+for t in range(T):
+    N = int(input())
+    for i in range(2,2080084):
+        if N % i != 0:
+            continue
+        if N % (i*i) == 0:
+            p = i
+            q = N // (p*p)
+            print(p,q)
+            break
+        else:
+            q = i
+            p = int(math.sqrt(N/q))
+            print(p,q)
+            break
 
