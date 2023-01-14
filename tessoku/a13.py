7522db86f272c9c422110c129fb8987695b881f5
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2023-01-14 15:15:41 +0900

    Accepted!

--- tessoku/a13.py
+++ tessoku/a13.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-
 N,K = map(int, input().split())
 A = list(map(int, input().split()))
+A.insert(0,0)
+R = [0] * (N+1)
 
+# しゃくとり法
+for i in range(1,N):
+    # スタート地点を決める
+    if i == 1:
+        R[i] = 1
+    else:
+        R[i] = R[i-1]
+
+    # ギリギリまで増やしていく
+    while (R[i] < N and A[R[i]+1]-A[i] <= K):
+        R[i] += 1
+
+# 出力
 ans = 0
-for i in range(N-1):
-    for j in range(i+1, N):
-        if A[j] - A[i] <= K:
-            ans += 1
-        else:
-            break
-
+for i in range(1,N):
+    ans += (R[i]-i)
 print(ans)
