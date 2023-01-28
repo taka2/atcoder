bad5cd7db091fbb53b55e4ed08122e8815f5ab48
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2023-01-28 22:46:01 +0900

    WA

--- abc287/d.py
+++ abc287/d.py
@@ -2,25 +2,44 @@
 S = input()
 T = input()
 
+def hantei(sindex, tindex):
+    if S[sindex] == '?' or T[tindex] == '?' or [sindex] == T[tindex]:
+        return True
+    else:
+        return False
+
 lenT = len(T)
+prevOk = -1
 for i in range(lenT+1):
     ans = True
-    # 先頭i文字のチェック
-    for j in range(i):
-        if S[j] == '?' or T[j] == '?' or S[j] == T[j]:
-            pass
-        else:
-            ans = False
-            break
-    # 残りの|T|-i文字のチェック
-    if ans:
+    if prevOk == -1:
+        prevOk = 0
+        # 初回ループ
         startIndex = i-lenT
         for j in range(lenT-i):
-            if S[startIndex+j] == '?' or T[j] == '?' or S[startIndex+j] == T[j]:
-                pass
+            if hantei(startIndex+j, j):
+                prevOk += 1
             else:
                 ans = False
-                break
+    else:
+        # 二周目以降
+        Ok = prevOk
+        # 新たに判定に加わる
+        if hantei(i-1, i-1):
+            Ok += 1
+        else:
+            Ok -= 1
+
+        # 判定から外れる
+        sindex = i-lenT-1
+        if hantei(sindex, i-1):
+            Ok -= 1
+        else:
+            Ok += 1
+
+        if Ok != lenT:
+            ans = False
+        prevOk = Ok
 
     if(ans):
         print("Yes")
