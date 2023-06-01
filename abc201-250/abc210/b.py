117f7bd49412992fbd3b951afbb144c7999dab44
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2023-06-02 07:30:38 +0900

    AC

---
+++ abc201-250/abc210/b.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+N = int(input())
+S = input()
+
+for i in range(N):
+    if S[i] == '1':
+        if i % 2 == 0:
+            print("Takahashi")
+            break
+        else:
+            print("Aoki")
+            break
