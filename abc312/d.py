92e4db0634fb53fac7b7e16c77f7c0298f40e154
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2023-07-31 22:15:26 +0900

    AC by PyPy3

--- abc312/d.py
+++ abc312/d.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 S = input()
+n = len(S)
 
-questionMarkCount = 0
-for i in range(len(S)):
-    if S[i] == '?':
-        questionMarkCount += 1
+# 0で埋めた二次元配列を用意する
+dp = [[0 for j in range(n+1)] for i in range(n+1)]
+
+dp[0][0] = 1
+for i in range(n):
+    ch = S[i]
+    for j in range(n):
+        if ch != ')':
+            dp[i+1][j+1] += dp[i][j]
+            dp[i+1][j+1] = dp[i+1][j+1] % 998244353
+        if j != 0 and ch != '(':
+            dp[i+1][j-1] += dp[i][j]
+            dp[i+1][j-1] = dp[i+1][j-1] % 998244353
+
+print(dp[n][0])
