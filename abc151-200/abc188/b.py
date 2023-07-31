67fa85a4672ca1da2da917be9013f21055c4b1f0
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2023-07-31 12:37:08 +0900

    AC

---
+++ abc151-200/abc188/b.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+sum = 0
+for i in range(N):
+    sum += A[i] * B[i]
+
+if sum == 0:
+    print("Yes")
+else:
+    print("No")
