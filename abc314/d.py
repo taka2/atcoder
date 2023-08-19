3675ec9bfa6fe1354442c891d8c5d28ce6544e0a
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2023-08-19 18:52:28 +0900

    TLE

--- abc314/d.py
+++ abc314/d.py
@@ -1,48 +1,33 @@
 # -*- coding: utf-8 -*-
 N = int(input())
 S = input()
+
+status = []
+for i in range(N):
+    status.append((0, S[i]))
+
 Q = int(input())
-txc = []
+
+fill = None
+
 for i in range(Q):
     t,x,c = input().split()
-    txc.append((int(t),int(x),c))
+    t = int(t)
+    x = int(x)
 
-listS = list(S)
-flag = 0 # 0はプレーン、1は小文字化、2は大文字化
-indexesAfterFlag = {}
-toLowerMap = {}
-toUpperMap = {}
-for i in range(26):
-    toLowerMap[chr(ord('A')+i)] = chr(ord('a')+i)
-    toLowerMap[chr(ord('a')+i)] = chr(ord('a')+i)
-    toUpperMap[chr(ord('a')+i)] = chr(ord('A')+i)
-    toUpperMap[chr(ord('A')+i)] = chr(ord('A')+i)
-
-for i in range(Q):
-    (t,x,c) = txc[i]
     if t == 1:
-        listS[x-1] = c
-        if flag == 1 or flag == 2:
-            indexesAfterFlag[x-1] = 1
-    elif t == 2:
-        flag = 1
-        indexesAfterFlag = {}
-    elif t == 3:
-        flag = 2
-        indexesAfterFlag = {}
-
-if flag == 1 or flag == 2:
-    for i in range(N):
-        if i in indexesAfterFlag:
-            pass
-        else:
-            if flag == 1:
-                listS[i] = toLowerMap[listS[i]]
-            elif flag == 2:
-                listS[i] = toUpperMap[listS[i]]
+        status[x-1] = (i,c)
+    else:
+        fill = (i,t)
 
 ans = ""
 for i in range(N):
-    ans += listS[i]
+    (time,c) = status[i]
+    if fill == None or time >= fill[0]:
+        ans += c
+    elif fill[1] == 2:
+        ans += c.lower()
+    else:
+        ans += c.upper()
 
 print(ans)
