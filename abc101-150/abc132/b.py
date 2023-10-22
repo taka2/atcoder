9bdbff683c651ca478c3c7b31a19c34de59edc5b
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2023-10-22 19:45:24 +0900

    AC

---
+++ abc101-150/abc132/b.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+n = int(input())
+p = list(map(int, input().split()))
+
+ans = 0
+for i in range(n-2):
+    if (p[i+1] > p[i] and p[i+1] < p[i+2]) or (p[i+1] < p[i] and p[i+1] > p[i+2]):
+        ans += 1
+
+print(ans)
