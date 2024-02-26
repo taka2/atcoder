fd549cded8f1aba2e8a4a1078903a5e24f1189bf
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2024-02-27 07:52:18 +0900

    AC

---
+++ abc001-050/abc021/a.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+N = int(input())
+
+if N==1:
+    print("1")
+    print("1")
+elif N==2:
+    print("1")
+    print("1")
+elif N==3:
+    print("2")
+    print("1")
+    print("2")
+elif N==4:
+    print("1")
+    print("4")
+elif N==5:
+    print("2")
+    print("1")
+    print("4")
+elif N==6:
+    print("2")
+    print("2")
+    print("4")
+elif N==7:
+    print("3")
+    print("1")
+    print("2")
+    print("4")
+elif N==8:
+    print("1")
+    print("8")
+elif N==9:
+    print("2")
+    print("1")
+    print("8")
+elif N==10:
+    print("2")
+    print("2")
+    print("8")
